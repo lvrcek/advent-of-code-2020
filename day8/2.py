12c4cdf32ba483a2ffd0f87bba1376534f07e480
Lovro Vrček <vrcek.lovro@gmail.com>
2020-12-09 11:06:44 +0800

    Refactored day 8

--- day8/2.py
+++ day8/2.py
@@ -7,7 +7,7 @@ Day 8, Part 2
 """
 
 
-def run(lines):
+def solve(lines):
     i = 0
     acc = 0
     used = set()
@@ -35,21 +35,19 @@ def main():
     with open('in.txt') as f:
         lines = f.readlines()
 
-    lines_2 = lines.copy()
     lines = [line.strip() for line in lines]
-    lines_2 = [line.strip() for line in lines_2]
 
     for i in range(len(lines)):
-        op, arg = lines[i].split()
-        if op == 'nop':
-            new_line = 'jmp ' + arg
-        elif op == 'jmp':
-            new_line = 'nop ' + arg
+        line = lines[i]
+        if line[:3] == 'nop':
+            tmp = 'jmp' + line[3:]
+        elif line[:3] == 'jmp':
+            tmp = 'nop' + line[3:]
         else:
             continue
-        lines_2[i] = new_line
-        is_done, acc = run(lines_2)
-        lines_2 = lines.copy()
+        lines[i] = tmp
+        is_done, acc = solve(lines)
+        lines[i] = line
         if is_done:
             break
 
